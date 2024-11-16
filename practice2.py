e419327ad8f17bd932cb30982402aa8cf7081629
2Golden44 <Bgreat0306@gmail.com>
2024-11-16 10:22:02 -0600

    Add files via upload

---
+++ practice2.py
@@ -0,0 +1,28 @@
+for counter in range(1,4) :
+    print(counter)
+    print("Go!")
+
+    Blue_team = ["Kim", "Lisa","Billy", "Kevin"]
+
+    print("The Blue Team members are:")
+    for player in blue_team:
+        print(player)
+
+
+        teams= ["Blue Team", "Kim","Lisa","Billy", "Kevin"], \
+               ["Red Team", "Greg","Maria","Chelsea", "Dave"], \
+               ["Green Team", "Chloe", "Ally", "Chase", "Donny"], \
+               
+for team in teams:
+      for name in team:
+           print(name)  
+           print("\n") 
+
+student  = {
+      "name":"Bob",
+      "age": 30,
+      "major": "Computer Science"
+ }
+
+
+
